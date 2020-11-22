b029d8b9424c24482cdca19baa113e96c3934c68
GeorgopoulosIoannis <ioannisgeor94@gmail.com>
2020-11-22 20:02:36 +0200

    Authorization microservice

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,8 +12,12 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
-
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signupRouter);
@@ -25,6 +30,9 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
@@ -36,7 +44,7 @@ const start = async () => {
     console.error(err);
   };
   app.listen(3000, () => {
-    console.log('Listening on Port 3000!!!');
+    console.log('Listening on Port 3000!!');
   });
 
 }
